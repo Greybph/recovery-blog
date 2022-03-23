8e606ba40d451248dde4a395a631ef2b26c49af3
andrew fauth <andrewlfauth@gmail.com>
2022-03-23 14:38:43 -0700

    tailwind

--- app/components/index/Hero.jsx
+++ app/components/index/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         Recovery is <span className='text-sky-600' id='recovery-text'>possible</span>
       </span>
       <span className='block pt-3 text-xl font-bold sm:text-2xl md:font-medium md:text-4xl lg:text-5xl'>It's not too late</span>
-      <p className='pt-5 text-xs font-medium leading-7 sm:max-w-lg md:leading-9 md:text-base '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit nisi earum nihil dicta fugit illo. Temporibus, rerum. Iusto asperiores repudiandae excepturi impedit est, tempora dolores?</p>
+      <p className='pt-5 text-xs font-medium leading-7 sm:max-w-lg md:leading-9 md:text-base '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit nisi earum nihil dicta fugit illo. Temporibus, rerum. Iusto asperiores repudiandae excepturi impedit est, tempora dolores talga?</p>
       <Link className='flex items-center px-3 py-2 mt-8 text-xs font-bold duration-300 border-2 border-black hover:bg-sky-600 hover:bg-opacity-30 hover:scale-105 w-fit' to='/'>
         Find out how
         <CgChevronRight className='ml-4 text-lg'/>
