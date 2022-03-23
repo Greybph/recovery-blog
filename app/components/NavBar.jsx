5c3eef8aae4d8c93a75a19b8cabe35f3f3b0e74c
andrew fauth <andrewlfauth@gmail.com>
2022-03-23 15:08:46 -0700

    rem size bug

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -3,13 +3,13 @@ import {Link} from 'remix'
 function NavBar() {
   return (
     <nav className='font-mont font-medium absolute top-0 flex items-center justify-between w-full px-4 py-4 bg-transparent md:px-12 lg:px-28'>
-      <span>LOGO</span>
+      <Link to='/'>LOGO</Link>
       <ul className='hidden space-x-16 md:flex'>
         <li>
           <Link to='/'>topics</Link>
         </li>
         <li>
-          <Link to='/'>blog</Link>
+          <Link to='/blog'>blog</Link>
         </li>
         <li>
           <Link to='/'>resources</Link>
