f8027444617cc8b62f810be6fb2553396975d2bf
andrew fauth <andrewlfauth@gmail.com>
2022-06-04 18:00:53 -0700

    test DB

--- app/utils/submitQuestion.server.js
+++ app/utils/submitQuestion.server.js
@@ -1,6 +1,12 @@
 import Questions from '~/models/Questions.server'
+import mongoose from './mongoose.server'
 
 export default async function submitQuestion(values) {
+  await mongoose.connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  
   const {email, question} = values
   Questions.create({email, question})
   return {sent: true}
