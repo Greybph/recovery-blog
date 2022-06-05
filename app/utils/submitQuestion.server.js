3d214de95f65a16a30ca3bbab85eccabd98ad20d
andrew fauth <andrewlfauth@gmail.com>
2022-06-04 18:13:14 -0700

    await create

--- app/utils/submitQuestion.server.js
+++ app/utils/submitQuestion.server.js
@@ -2,6 +2,6 @@ import Questions from '~/models/Questions.server'
 
 export default async function submitQuestion(values) {
   const {email, question} = values
-  Questions.create({email, question})
+  await Questions.create({email, question})
   return {sent: true}
 }
