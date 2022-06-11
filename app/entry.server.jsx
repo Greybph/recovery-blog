342b21225ede684466b28460bb637216f096a3d5
andrew fauth <andrewlfauth@gmail.com>
2022-06-10 20:20:27 -0700

    canonical response header

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -7,11 +7,19 @@ export default function handleRequest(
   responseHeaders,
   remixContext
 ) {
+
+  const url = new URL(request.url)
+  const urlArray = url.pathname.split("/")
+
   const markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
   );
 
   responseHeaders.set("Content-Type", "text/html");
+
+  if (urlArray.length === 3 && urlArray[1] === 'blog') {
+    responseHeaders.set("Link", `<https://recoveryocean.com/blog/${urlArray[2]}>; rel="canonical"`)
+  }
 
   return new Response("<!DOCTYPE html>" + markup, {
     status: responseStatusCode,
