00abce84837080e19dfe47f59a283771351c1b72
andrew fauth <andrewlfauth@gmail.com>
2022-06-12 16:14:19 -0700

    blog post card image width

--- app/postDex.js
+++ app/postDex.js
@@ -5,7 +5,7 @@ const postDex = [
     description: "Sometimes the most damaging lies an addict tells are to themself.",
     category: "addiction",
     to: "/blog/3-lies-addicts-tell-themselves",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654553208/man-struggling-with-his-thoughts_xofe0g.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654553208/man-struggling-with-his-thoughts_xofe0g.jpg',
     alt: "man sitting on chair covering his eyes",
   },
   {
@@ -14,7 +14,7 @@ const postDex = [
     description: "This is one that I would love to forget.",
     category: "addiction",
     to: "/blog/the-most-humiliating-moment-of-my-drug-addiction",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654555339/lava_zqtz2p.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555339/lava_zqtz2p.jpg',
   },
   {
     id: 5,
@@ -22,7 +22,7 @@ const postDex = [
     description: "Not your typically storybook romance. Can addicts actually fall in love?",
     category: "addiction",
     to: "/blog/do-drug-addicts-fall-in-love",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654555428/heart_akzboj.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555428/heart_akzboj.jpg',
     attribute: "Photo by Belinda Fewings on Unsplash",
   },
   {
@@ -31,7 +31,7 @@ const postDex = [
     description: "Find out the rules for participation in an NA meeting.",
     category: "recovery",
     to: "/blog/do-you-have-to-talk-at-na-meetings",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654555504/na-meeting_lkczdn.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555504/na-meeting_lkczdn.jpg',
     attribute: "By Sincerely Media on Unsplash"
   },
   {
@@ -40,7 +40,7 @@ const postDex = [
     description: "Drugs turn us into completely different people. Are those people always mean?",
     category: "addiction",
     to: "/blog/are-drug-addicts-mean",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654555530/aggression_peqzde.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555530/aggression_peqzde.jpg',
   },
   {
     id: 8,
@@ -48,7 +48,7 @@ const postDex = [
     description: "Learn how to deal with fear of missing out when you stop using.",
     category: "recovery",
     to: "/blog/fomo-in-early-recovery",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654555564/fearful_lucktp.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555564/fearful_lucktp.jpg',
   },
   {
     id: 9,
@@ -56,7 +56,7 @@ const postDex = [
     description: "Stealing copper..? Seems like too much work.",
     category: "addiction",
     to: "/blog/why-do-drug-addicts-steal-copper",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654555613/copper-pile_cfl9yz.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555613/copper-pile_cfl9yz.jpg',
   },
   {
     id: 10,
@@ -64,7 +64,7 @@ const postDex = [
     description: "Is that even allowed?",
     category: "addiction",
     to: "/blog/do-drug-addicts-donate-plasma",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654555652/blood-cells_ngi9rb.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555652/blood-cells_ngi9rb.jpg',
   },
   {
     id: 11,
@@ -72,7 +72,7 @@ const postDex = [
     description: "Find out how I was able to successfully get off of heroin after eight years of addiction.",
     category: "recovery",
     to: "/blog/how-i-managed-to-stop-using-heroin",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654555825/poison_k9xemi.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555825/poison_k9xemi.jpg',
   },
   {
     id: 12,
@@ -80,7 +80,7 @@ const postDex = [
     description: "How to handle unexpected run-ins with people you used to get high with.",
     category: "recovery",
     to: "/blog/encountering-addicts-from-our-past",
-    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/v1654800839/clock_ke5hwm.jpg',
+    image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654800839/clock_ke5hwm.jpg',
   },
  
 ]
