8dc076b98005eca68dd2e2ec3d4a5586ecae91a9
andrew fauth <andrewlfauth@gmail.com>
2022-06-28 14:59:52 -0700

    added suggestions to each post

--- app/components/blog/EndCTA.jsx
+++ app/components/blog/EndCTA.jsx
@@ -2,27 +2,23 @@ import postDex from '~/postDex'
 import BlogPostCard from './BlogPostCard'
 import {Link, useTransition, useLocation} from 'remix'
 import {BsArrowRight} from 'react-icons/bs'
-import {useRef} from 'react'
-
 
 function EndCTA() {
   const transition = useTransition()
   const slug = useLocation().pathname
-  const addictionSuggestions = 
-    postDex.filter(post => post.to !== slug && post.category === "addiction")
-  const recoverySuggestions = 
-    postDex.filter(post => post.to !== slug && post.category === "recovery")
-
-  const idx1 = useRef(Math.floor(Math.random() * addictionSuggestions.length))
-  const idx2 = useRef(Math.floor(Math.random() * recoverySuggestions.length))
-
+  const post = postDex.filter(post => post.to === slug)[0]
+  
   return (
     <div className="px-4 pt-12 -mb-10 border-t md:pt-20 md:-mb-0 not-prose md:px-0">
       <span className="block mb-4 text-4xl font-bold tracking-tight font-mont">Don't stop now!</span>
       <p className="text-lg ">Check out these posts for more insight into drug addiction and recovery.</p>
+     
       <div className='grid-cols-2 gap-10 pt-6 md:pt-10 md:grid'>
-        <BlogPostCard post={addictionSuggestions[idx1.current]} />
-        <BlogPostCard post={recoverySuggestions[idx2.current]} />
+        {post.suggest.map(s => 
+          <BlogPostCard key={s} post={postDex.find(post => 
+            post.id === s)} 
+          />
+        )}
       </div>
 
       <Link to='/posts' className="flex items-center justify-between mx-auto mt-10 font-medium rounded-md md:mt-14 group w-fit md:text-lg">
--- app/postDex.js
+++ app/postDex.js
@@ -7,6 +7,7 @@ const postDex = [
     to: "/blog/3-lies-addicts-tell-themselves",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654553208/man-struggling-with-his-thoughts_xofe0g.jpg',
     alt: "man sitting on chair covering his eyes",
+    suggest: [14,5] 
   },
   {
     id: 4,
@@ -15,6 +16,7 @@ const postDex = [
     category: "addiction",
     to: "/blog/the-most-humiliating-moment-of-my-drug-addiction",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555339/lava_zqtz2p.jpg',
+    suggest: [6,11]
   },
   {
     id: 5,
@@ -24,6 +26,7 @@ const postDex = [
     to: "/blog/do-drug-addicts-fall-in-love",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555428/heart_akzboj.jpg',
     attribute: "Photo by Belinda Fewings on Unsplash",
+    suggest: [7,9]
   },
   {
     id: 6,
@@ -32,7 +35,8 @@ const postDex = [
     category: "recovery",
     to: "/blog/do-you-have-to-talk-at-na-meetings",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555504/na-meeting_lkczdn.jpg',
-    attribute: "By Sincerely Media on Unsplash"
+    attribute: "By Sincerely Media on Unsplash",
+    suggest: [12,13]
   },
   {
     id: 7,
@@ -41,6 +45,7 @@ const postDex = [
     category: "addiction",
     to: "/blog/are-drug-addicts-mean",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555530/aggression_peqzde.jpg',
+    suggest: [10,9]
   },
   {
     id: 8,
@@ -49,6 +54,7 @@ const postDex = [
     category: "recovery",
     to: "/blog/fomo-in-early-recovery",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555564/fearful_lucktp.jpg',
+    suggest: [14,3]
   },
   {
     id: 9,
@@ -57,6 +63,7 @@ const postDex = [
     category: "addiction",
     to: "/blog/why-do-drug-addicts-steal-copper",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555613/copper-pile_cfl9yz.jpg',
+    suggest: [13,7]
   },
   {
     id: 10,
@@ -65,6 +72,7 @@ const postDex = [
     category: "addiction",
     to: "/blog/do-drug-addicts-donate-plasma",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555652/blood-cells_ngi9rb.jpg',
+    suggest: [7,9]
   },
   {
     id: 11,
@@ -73,6 +81,7 @@ const postDex = [
     category: "recovery",
     to: "/blog/how-i-managed-to-stop-using-heroin",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654555825/poison_k9xemi.jpg',
+    suggest: [14,7]
   },
   {
     id: 12,
@@ -81,6 +90,7 @@ const postDex = [
     category: "recovery",
     to: "/blog/encountering-addicts-from-our-past",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1654800839/clock_ke5hwm.jpg',
+    suggest: [15,8]
   },
   {
     id: 13,
@@ -89,6 +99,7 @@ const postDex = [
     category: "recovery",
     to: "/blog/na/virtual-na-meetings-beginners-guide",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1655857512/video-conference-g311ca2ef0_640_jfrfgt.png',
+    suggest: [14,6]
   },
   {
     id: 14,
@@ -97,6 +108,7 @@ const postDex = [
     category: "recovery",
     to: "/blog/na/12-steps-of-na-cheat-sheet",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1655939061/carousel-g6de194375_640_rvxkrk.png',
+    suggest: [15,5]
   },
   {
     id: 15,
@@ -105,6 +117,7 @@ const postDex = [
     category: "recovery",
     to: "/blog/na/12-traditions-of-na",
     image: 'https://res.cloudinary.com/recovery-ocean/image/upload/w_305/v1656106472/rules-1752415_640_oef0ej.jpg',
+    suggest: [13,3]
   },
 ]
 
