fab6c0d83a29d2410afca97e920360e26a27457b
andrew fauth <andrewlfauth@gmail.com>
2022-06-30 10:17:17 -0700

    shuffle posts and random post btns

--- app/components/blog/PostsSection.jsx
+++ app/components/blog/PostsSection.jsx
@@ -2,10 +2,34 @@ import BlogPostCard from '~/components/blog/BlogPostCard'
 import postDex from '~/postDex'
 import {useState, useEffect} from 'react'
 import {gsap} from 'gsap'
+import {Link, useTransition} from 'remix'
 import CategoryButton from './CategoryButton'
+import {BsShuffle} from 'react-icons/bs'
+import {GiPerspectiveDiceSixFacesRandom} from 'react-icons/gi'
 
 function PostsSection({filter= "all"}) {
+  const transition = useTransition()
   const [category, setCategory] = useState(filter)
+  const [shuffle, setShuffle] = useState(false)
+  const [posts, setPosts] = useState(postDex)
+  const [randomPostPath, setRandomPostPath] = useState(posts[Math.floor(Math.random() * posts.length)].to)
+  const shuffleArray = (array) => {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
+  useEffect(() => {
+    if (shuffle) {
+      setPosts(shuffleArray(posts))
+      setShuffle(false)
+    }
+  }, [shuffle, posts])
 
   useEffect(() => {
     if (window.scrollY > 370) window.scrollTo(
@@ -15,19 +39,24 @@ function PostsSection({filter= "all"}) {
   }, [category])
 
   useEffect(() => {
-    if (category === 'all')
-      gsap.from('#cb-all', {y:20, duration: 0.3, stagger: 0.1})
-    
-    if (category === 'addiction')
-      gsap.from('#cb-addiction', {y:20, duration: 0.3, stagger: 0.1})
-    
-    if (category === 'recovery')
-      gsap.from('#cb-recovery', {y:20, duration: 0.3, stagger: 0.1})
-    
+    gsap.from(`#cb-${category}`, {y: 20, duration: 0.3, stagger: 0.1})
   }, [category])
+
+  useEffect(() => {
+    setRandomPostPath(posts[Math.floor(Math.random() * posts.length)].to)
+  }, [posts, category])
 
   return (
     <main className='px-4 pb-20 md:px-10 lg:px-28 xl:px-32'>
+      <div className="flex mb-4 space-x-4 md:justify-center md:-mt-8 lg:justify-start lg:-mt-10">
+        <button title="shuffle" aria-label="shuffle posts" onClick={() => setShuffle(true)} className="p-2 duration-100 border rounded-md shadow-md lg:hover:scale-105">
+          <BsShuffle className="text-2xl text-blue-500 lg:text-2xl" />
+        </button>
+        <Link to={randomPostPath} title="random" aria-label="random post" className='p-2 duration-100 border rounded-md shadow-md lg:hover:scale-105'>
+          <GiPerspectiveDiceSixFacesRandom className={`${transition.state === 'loading' && transition.location.pathname === randomPostPath ? 'animate-spin' : ''} text-2xl text-blue-500 lg:text-2xl`} />
+        </Link>
+      </div>
+      
       <div className="flex justify-center mb-8 space-x-4 text-sm md:mb-10 lg:justify-start font-mont">
         <CategoryButton label="all" category={category} onClick={() => setCategory("all")} />
         <CategoryButton label="addiction" category={category} onClick={() => setCategory("addiction")} />
@@ -36,34 +65,30 @@ function PostsSection({filter= "all"}) {
       
       <div className="justify-center md:flex lg:block">
         <div className='md:grid grid-cols-2 lg:grid-cols-3 gap-12 lg:gap-10 lg:w-[80vw] xl:w-[70vw] space-y-8 md:space-y-0'>
-          {category === "all" && postDex.map(post => (
-            <BlogPostCard 
-            key={post.id}
-            post={post}
-            onClick={
-              post.category === "addiction" ?
-              () => setCategory("addiction") :
-              () => setCategory("recovery") 
-            }
-          />
-          ))}
           
-          {category === "addiction" && postDex.filter(post => post.category === "addiction").map(post => (
-            <BlogPostCard 
-            key={post.id}
-            post={post}
-            onClick={() => setCategory("addiction")}
-          />
-          ))}
-          
-          {category === "recovery" && postDex.filter(post => post.category === "recovery").map(post => (
-            <BlogPostCard 
-            key={post.id}
-            post={post}
-            onClick={() => setCategory("recovery")}
-          />
-          ))}
-
+          {category === "all" ? 
+            posts.map(post => (
+              <BlogPostCard 
+                key={post.id}
+                post={post}
+                onClick={
+                  post.category === "addiction" ?
+                  () => setCategory("addiction") :
+                  () => setCategory("recovery") 
+                }
+              />)) 
+          : 
+            posts.filter(post => post.category === category).map(post => (
+              <BlogPostCard 
+                key={post.id}
+                post={post}
+                onClick={
+                  post.category === "addiction" ?
+                  () => setCategory("addiction") :
+                  () => setCategory("recovery") 
+                }
+              />))
+          }
 
         </div>
       </div>
